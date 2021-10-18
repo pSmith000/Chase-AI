d07f7c0e01ba17a29715b64c8c7ca6917764b17d
pSmith000 <88592701+pSmith000@users.noreply.github.com>
2021-10-18 15:23:03 -0500

    Initialize Raylib

---
+++ MathForGames/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class Player : Actor
+    {
+        private float _speed;
+        private Vector2 _velocity;
+
+        public float Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public Vector2 Velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
+
+        public Player(char icon, float x, float y, float speed, Color color, string name = "Actor") 
+            : base(icon, x, y, color, name)
+        {
+            _speed = speed;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            //Get the player input direction
+            int xDirection = -Convert.ToInt32(Raylib.IsKeyDown(KeyboardKey.KEY_A))
+                + Convert.ToInt32(Raylib.IsKeyDown(KeyboardKey.KEY_D));
+            int yDirection = -Convert.ToInt32(Raylib.IsKeyDown(KeyboardKey.KEY_W))
+                + Convert.ToInt32(Raylib.IsKeyDown(KeyboardKey.KEY_S));
+
+            //Create a vector that stores the move input
+            Vector2 moveDirection = new Vector2(xDirection, yDirection);
+
+            Velocity = moveDirection.Normalized * Speed * deltaTime;
+
+            Position += Velocity;
+
+            base.Update(deltaTime);
+        }
+
+        public override void OnCollision(Actor actor)
+        {
+            Console.WriteLine("Collision occured");
+        }
+    }
+}
